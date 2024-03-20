cb3b89bcbdac8ce15f6bf26858c2f96c46c1caf2
jacashira <jacashira@gmail.com>
2024-03-20 11:57:11 -0600

    Implementacion de libreria nodemailer

--- nodemailer.js
+++ nodemailer.js
@@ -1,34 +1,26 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-// async..await is not allowed in global scope, must use a wrapper
-async function sendMail() {
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    secure: true, // true for 465, false for other ports
-    port: 465,
+// Create a transporter object using SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-      user: 'nicobytes.demo@gmail.com',
-      pass: 'dmlikrjugujjlugl'
+        user: 'jacashira@gmail.com', // Your Gmail address
+        pass: 'ilod ngwy gznw drcr' // Your Gmail password
     }
-  });
+});
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: 'nicobytes.demo@gmail.com', // sender address
-    to: "santibytes.demo@gmail.com", // list of receivers
-    subject: "Este es un nuevo correo", // Subject line
-    text: "Hola santi", // plain text body
-    html: "<b>Hola santi</b>", // html body
-  });
+// Set up email data
+let mailOptions = {
+    from: 'jacashira@gmail.com', // Sender address
+    to: 'jacashira@gmail.com', // Receiver address
+    subject: 'Test Email', // Subject line
+    text: 'Hello from Nodemailer!' // Plain text body
+};
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-sendMail();
+// Send email
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+});
